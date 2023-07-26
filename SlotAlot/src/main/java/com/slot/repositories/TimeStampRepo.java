655aeefe15423f04d66e7445b5db9582f4739fd7
Yuvraj30903 <136421732+Yuvraj30903@users.noreply.github.com>
2023-07-26 22:50:42 +0530

    Delete SlotAlot directory

--- SlotAlot/src/main/java/com/slot/repositories/TimeStampRepo.java
+++
@@ -1,9 +0,0 @@
-package com.slot.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.slot.entities.TimeStamp;
-
-public interface TimeStampRepo extends JpaRepository<TimeStamp, String>{
-
-}
